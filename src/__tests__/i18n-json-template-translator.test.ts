dfc439078462552ed45cd1d6c7306024e9e8e24d
Seyyedmahdi <smahdi1991@gmail.com>
2020-05-05 23:29:56 +0430

    add fp based on ramda

--- src/__tests__/i18n-json-template-translator.test.ts
+++ src/__tests__/i18n-json-template-translator.test.ts
@@ -1,8 +1,8 @@
-import i18nTranslatorForJsonTemplates from '../index';
+import i18nTranslatorForJsonTemplates2 from '../index';
 
 const lang = require('./lang.json');
 const template = require('./json_template.json');
 const result = require('./result.json');
 test('i18nTranslatorForJsonTemplates', () => {
-  expect(i18nTranslatorForJsonTemplates(lang, template)).toEqual(result);
+  expect(i18nTranslatorForJsonTemplates2(lang, template)).toEqual(result);
 });
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,42 @@
   email:seyyedkhandon@gmail.com
 */
 
-const path = require('lodash/fp/path');
+// const path = require('lodash/fp/path');
+// const prepareRegexPattern = (start: string, end: string) => new RegExp(`${start}[\\w.]+${end}`);
+// const preparePatternRemoverFromText = (start: string, end: string) => (text: string) =>
+//   text.slice(start.length, text.length - end.length);
+// const i18nTranslatorForJsonTemplates = (
+//   language_json: any,
+//   template_json: any,
+//   start_of_pattern = '{{',
+//   end_of_pattern = '}}',
+// ) => {
+//   const pattern = prepareRegexPattern(start_of_pattern, end_of_pattern);
+//   const patternRemover = preparePatternRemoverFromText(start_of_pattern, end_of_pattern);
+//   const dfs = (tree: any) => {
+//     if (typeof tree === 'object') {
+//       Object.keys(tree).forEach((key: string) => {
+//         tree[key] = dfs(tree[key]);
+//       });
+//     } else if (Array.isArray(tree)) {
+//       tree.forEach((branch: any, index: number) => {
+//         tree[index] = dfs(branch);
+//       });
+//     } else if (typeof tree === 'string') {
+//       if (tree.match(pattern)) {
+//         tree = path(patternRemover(tree), language_json);
+//       }
+//     }
+//     return tree;
+//   };
+//   return dfs(template_json);
+// };
+const R = require('ramda');
 const prepareRegexPattern = (start: string, end: string) => new RegExp(`${start}[\\w.]+${end}`);
-const preparePatternRemoverFromText = (start: string, end: string) => (text: string) =>
-  text.slice(start.length, text.length - end.length);
-const i18nTranslatorForJsonTemplates = (
+const preparePatternRemoverFromText = (start: string, end: string) =>
+  R.pipe(R.drop(start.length), R.dropLast(end.length));
+const path = R.curry((keys: string, obj: any) => R.pipe(R.split('.'), R.path(keys, obj)));
+const i18nTranslatorForJsonTemplates2 = (
   language_json: any,
   template_json: any,
   start_of_pattern = '{{',
@@ -17,22 +48,21 @@ const i18nTranslatorForJsonTemplates = (
   const pattern = prepareRegexPattern(start_of_pattern, end_of_pattern);
   const patternRemover = preparePatternRemoverFromText(start_of_pattern, end_of_pattern);
   const dfs = (tree: any) => {
-    if (typeof tree === 'object') {
-      Object.keys(tree).forEach((key: string) => {
-        tree[key] = dfs(tree[key]);
-      });
-    } else if (Array.isArray(tree)) {
-      tree.forEach((branch: any, index: number) => {
-        tree[index] = dfs(branch);
-      });
-    } else if (typeof tree === 'string') {
-      if (tree.match(pattern)) {
-        tree = path(patternRemover(tree), language_json);
-      }
+    switch (R.type(tree)) {
+      case 'Object':
+      case 'Array':
+        return R.map(dfs, tree);
+      case 'String':
+        if (tree.match(pattern)) {
+          return R.pipe(patternRemover, R.split('.'), (_: string) => R.path(_, language_json))(tree);
+        }
+        break;
+      default:
+        return tree;
     }
     return tree;
   };
   return dfs(template_json);
 };
 
-export default i18nTranslatorForJsonTemplates;
+export default i18nTranslatorForJsonTemplates2;
