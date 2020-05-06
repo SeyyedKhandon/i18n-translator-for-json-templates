81e4f8fe2e852483f1079ef38ce703e219720085
Seyyedmahdi <smahdi1991@gmail.com>
2020-05-06 16:10:48 +0430

    remove lodash and lodash version comments

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,6 @@
   email:seyyedkhandon@gmail.com
 */
 
-// const path = require('lodash/fp/path');
-// const prepareRegexPattern = (start: string, end: string) => new RegExp(`${start}[\\w.]+${end}`);
-// const preparePatternRemoverFromText = (start: string, end: string) => (text: string) =>
-//   text.slice(start.length, text.length - end.length);
-// const i18nTranslatorForJsonTemplates = (
-//   language_json: any,
-//   template_json: any,
-//   start_of_pattern = '{{',
-//   end_of_pattern = '}}',
-// ) => {
-//   const pattern = prepareRegexPattern(start_of_pattern, end_of_pattern);
-//   const patternRemover = preparePatternRemoverFromText(start_of_pattern, end_of_pattern);
-//   const dfs = (tree: any) => {
-//     if (typeof tree === 'object') {
-//       Object.keys(tree).forEach((key: string) => {
-//         tree[key] = dfs(tree[key]);
-//       });
-//     } else if (Array.isArray(tree)) {
-//       tree.forEach((branch: any, index: number) => {
-//         tree[index] = dfs(branch);
-//       });
-//     } else if (typeof tree === 'string') {
-//       if (tree.match(pattern)) {
-//         tree = path(patternRemover(tree), language_json);
-//       }
-//     }
-//     return tree;
-//   };
-//   return dfs(template_json);
-// };
 const R = require('ramda');
 const prepareRegexPattern = (start: string, end: string) => new RegExp(`${start}[\\w.]+${end}`);
 const preparePatternRemoverFromText = (start: string, end: string) =>
